e85e917c3ddb73bde2ee9b8310f973badffe0a5e
Marco Salas <marco@salas.cloud>
2023-02-04 16:10:26 +0100

    Add Player Movement

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    [SerializeField] private Animator _animator;
-    [SerializeField] private Rigidbody 
+    [SerializeField] private Animator animator;
+    [SerializeField] private Rigidbody2D rigidbody2D;
     
+    [SerializeField] private float speed = 5;
+
+    [SerializeField] private bool isLocalPlayer = false;
     
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +22,51 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isLocalPlayer)
+        {
+            // Get the horizontal and vertical input
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+
+
+            UpdateMovement(horizontal, vertical);
         
+            // Attack on mouse down
+            if (Input.GetMouseButtonDown(0))
+            {
+                ExecuteAttack();
+            }
+        }
+
+
+        UpdateAnimatorParameters();
+    }
+    
+    void UpdateAnimatorParameters()
+    {
+        if(rigidbody2D.velocity.magnitude > 1)
+        {
+            animator.SetBool("IsWalking", true);
+        }
+        else
+        {
+            animator.SetBool("IsWalking", false);
+        }
+    }
+    
+    
+    void UpdateMovement(float horizontal, float vertical)
+    {
+        // Set the animator parameters
+        animator.SetFloat("X", horizontal);
+        animator.SetFloat("Y", vertical);
+        
+        // Set the rigidbody velocity
+        rigidbody2D.velocity = new Vector2(horizontal, vertical) * speed;
+    }
+
+    void ExecuteAttack()
+    {
+        animator.SetTrigger("Attack");
     }
 }
